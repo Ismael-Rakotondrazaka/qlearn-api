f6ca0e5da6561285794a106d6ea8ace259bafe0a
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-07-13 14:58:07 +0300

    refactor: :art: Add an instance of the CategoryRepository in the RepoProvider.

--- server/utils/providers/repositoryProvider.ts
+++ server/utils/providers/repositoryProvider.ts
@@ -1,8 +1,13 @@
 import { prismaClient } from "../prisma";
 import { UserRepository } from "../repositories";
+import { CategoryRepository } from "../repositories/categories";
 
 export abstract class RepositoryProvider {
   public static userRepository: UserRepository = new UserRepository(
+    prismaClient,
+  );
+
+  public static categoryRepository: CategoryRepository = new CategoryRepository(
     prismaClient,
   );
 }
