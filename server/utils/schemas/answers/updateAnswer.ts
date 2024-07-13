5369a1c228a7f2947ff36c72857d8f6edd79d518
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-07-13 19:59:41 +0300

    feat: :sparkles: Define update Answer Request.

---
+++ server/utils/schemas/answers/updateAnswer.ts
@@ -0,0 +1,32 @@
+import type { Simplify } from "type-fest";
+import { z } from "zod";
+import type { AnswerDTO } from "../../dtos";
+import type { Request } from "../../requests";
+import type { ResponseError } from "../../responses";
+
+
+export const UpdateAnswerParamsSchema = z.object({
+  id: z.coerce.number(),
+});
+
+
+export const UpdateAnswerBodySchema = z.object({
+  content: z.string().min(1)
+});
+
+
+export type UpdateAnswerParams = z.infer<typeof UpdateAnswerParamsSchema>;
+export type UpdateAnswerBody = z.infer<typeof UpdateAnswerBodySchema>;
+
+export type UpdateAnswerData = {
+  answer: AnswerDTO;
+};
+
+export type UpdateAnswerRequest = Request<
+  UpdateAnswerData,
+  UpdateAnswerBody,
+  UpdateAnswerParams
+>;
+
+export type UpdateAnswerError = Simplify<ResponseError<UpdateAnswerRequest>>;
+export type UpdateAnswerErrorData = UpdateAnswerError["data"];
